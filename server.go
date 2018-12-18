38ce48257d932389b9c5139e0d968d77346b3a82
Aleksandr Kuzmin <grinova@gmail.com>
2018-12-18 13:42:56 +0300

    Reset game state after score

--- server.go
+++ server.go
@@ -5,19 +5,24 @@ import (
 	"log"
 	"math"
 
+	"github.com/grinova/classic2d-server/vmath"
 	physicsnet "github.com/grinova/physicsnet-server"
 )
 
 var shipProps = map[string]physicsnet.BodyCreateProps{
 	"ship-a": physicsnet.BodyCreateProps{
-		ID:       "ship-a",
-		Position: physicsnet.Point{X: -0.5, Y: -0.5},
-		Angle:    0,
+		ID:              "ship-a",
+		Position:        physicsnet.Point{X: -0.5, Y: -0.5},
+		Angle:           0,
+		LinearVelocity:  physicsnet.Point{X: 0, Y: 0},
+		AngularVelocity: 0,
 	},
 	"ship-b": physicsnet.BodyCreateProps{
-		ID:       "ship-b",
-		Position: physicsnet.Point{X: 0.5, Y: 0.5},
-		Angle:    math.Pi,
+		ID:              "ship-b",
+		Position:        physicsnet.Point{X: 0.5, Y: 0.5},
+		Angle:           math.Pi,
+		LinearVelocity:  physicsnet.Point{X: 0, Y: 0},
+		AngularVelocity: 0,
 	},
 }
 
@@ -31,21 +36,26 @@ type scoreProps struct {
 	Amount     int    `json:"amount"`
 }
 
+type player struct {
+	name  string
+	score int
+}
+
 // Server - сервер игры
 type Server struct {
 	physicsnet.Server
-	clients map[string]string
+	players map[string]*player
 }
 
 // Start - запуск сервера
 func (server *Server) Start() {
-	server.clients = make(map[string]string)
+	server.players = make(map[string]*player)
 	listener := physicsnet.ServerListener{
 		OnServerStart: func(s *physicsnet.Server) {
 			s.Props.NewID = func() (string, error) {
 				for i := 'a'; i < 'z'; i++ {
 					id := "ship-" + string(i)
-					if _, ok := server.clients[id]; !ok {
+					if _, ok := server.players[id]; !ok {
 						return id, nil
 					}
 				}
@@ -72,27 +82,25 @@ func (server *Server) Start() {
 		OnServerStop: func(s *physicsnet.Server) {
 			log.Println("Server stop")
 		},
-		OnClientConnect: func(s *physicsnet.Server, id string, c *physicsnet.Client) error {
-			if props, ok := shipProps[id]; ok {
-				s.CreateEntity(id, "ship", props)
-				c.SendSystemMessage(systemProps{Type: "user-name", Data: id})
-				server.clients[id] = ""
-				log.Printf("Client connect: id = %s\n", id)
-				return nil
+		OnClientConnect: func(s *physicsnet.Server, id string, client *physicsnet.Client) error {
+			if ok := server.createShip(id); !ok {
+				return fmt.Errorf("OnClientConnect: can't create ship with id = `%s`", id)
 			}
-			return fmt.Errorf("onClientConnect: didn't find initial properties for ship id `%s`", id)
+			client.SendSystemMessage(systemProps{Type: "user-name", Data: id})
+			server.players[id] = &player{}
+			log.Printf("Client connect: id = %s\n", id)
+			return nil
 		},
 		OnClientDisconnect: func(s *physicsnet.Server, id string) {
-			opponentName := server.clients[id]
-			for clientID := range server.clients {
-				if clientID != id {
-					c := s.GetClient(clientID)
-					c.SendSystemMessage(systemProps{Type: "opponent-leave", Data: opponentName})
+			s.DestroyEntity(id)
+			player := server.players[id]
+			delete(server.players, id)
+			for playerID, player := range server.players {
+				if client := s.GetClient(playerID); client != nil {
+					client.SendSystemMessage(systemProps{Type: "opponent-leave", Data: player.name})
 				}
 			}
-			delete(server.clients, id)
-			s.DestroyEntity(id)
-			log.Printf("Client disconnect: id = %s\n", id)
+			log.Printf("Client disconnect: name = %s, id = %s\n", player.name, id)
 		},
 		OnEventMessage: func(s *physicsnet.Server, id string, m interface{}) bool {
 			log.Printf("Event from %s: %s\n", id, m)
@@ -101,20 +109,8 @@ func (server *Server) Start() {
 		OnSystemMessage: func(s *physicsnet.Server, id string, m interface{}) bool {
 			if data, ok := m.(map[string]interface{}); ok {
 				if data["type"] == "player-name" {
-					if opponentName, ok := data["data"].(string); ok {
-						server.clients[id] = opponentName
-						if len(server.clients) > 1 {
-							opponent := s.GetClient(id)
-							for clientID, clientName := range server.clients {
-								if clientID != id {
-									c := s.GetClient(clientID)
-									if c != nil {
-										c.SendSystemMessage(systemProps{Type: "opponent-join", Data: opponentName})
-									}
-									opponent.SendSystemMessage(systemProps{Type: "opponent-join", Data: clientName})
-								}
-							}
-						}
+					if playerName, ok := data["data"].(string); ok {
+						server.onReceivePlayerName(id, playerName)
 					}
 				}
 			}
@@ -126,6 +122,26 @@ func (server *Server) Start() {
 	go server.Loop()
 }
 
+func (server *Server) createShip(id string) bool {
+	if props, ok := shipProps[id]; ok {
+		server.CreateEntity(id, "ship", props)
+		return true
+	}
+	return false
+}
+
+func (server *Server) resetShip(id string) bool {
+	if props, ok := shipProps[id]; ok {
+		body := server.GetBody(id)
+		body.Sweep.A = props.Angle
+		body.Sweep.C = vmath.Vec2{X: props.Position.X, Y: props.Position.Y}
+		body.LinearVelocity = vmath.Vec2{X: props.LinearVelocity.X, Y: props.LinearVelocity.Y}
+		body.AngularVelocity = props.AngularVelocity
+		return true
+	}
+	return false
+}
+
 func (server *Server) onDestroyShip(destroyerID string, destroyedID string) {
 	var amount int
 	if destroyerID == destroyedID {
@@ -133,9 +149,37 @@ func (server *Server) onDestroyShip(destroyerID string, destroyedID string) {
 	} else {
 		amount = 1
 	}
-	playerName := server.clients[destroyerID]
-	for clientID := range server.clients {
-		c := server.GetClient(clientID)
-		c.SendSystemMessage(systemProps{Type: "score", Data: scoreProps{PlayerName: playerName, Amount: amount}})
+	if player, ok := server.players[destroyerID]; ok {
+		for playerID := range server.players {
+			c := server.GetClient(playerID)
+			player.score++
+			c.SendSystemMessage(systemProps{Type: "score", Data: scoreProps{PlayerName: player.name, Amount: amount}})
+		}
+	}
+	for playerID, player := range server.players {
+		if playerID == destroyedID {
+			server.createShip(destroyedID)
+		} else {
+			server.resetShip(playerID)
+		}
+		player.score = 0
+	}
+}
+
+func (server *Server) onReceivePlayerName(playerID string, playerName string) {
+	if player, ok := server.players[playerID]; ok {
+		player.name = playerName
+		if len(server.players) > 1 {
+			if client := server.GetClient(playerID); client != nil {
+				for id, player := range server.players {
+					if playerID != id {
+						if c := server.GetClient(id); c != nil {
+							c.SendSystemMessage(systemProps{Type: "opponent-join", Data: playerName})
+						}
+						client.SendSystemMessage(systemProps{Type: "opponent-join", Data: player.name})
+					}
+				}
+			}
+		}
 	}
 }
